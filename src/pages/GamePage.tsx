cd742ced74170643fff9367d8b47ec51798f0e1c
Denys Topchyi <miliarder97@gmail.com>
2023-08-22 23:26:14 +0300

    cards page were added

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom"
+import { ROUTE_BOOK_CARD, ROUTE_FLIP_CARD } from "../constants/constants"
+
 export const GamePage = () => {
   return (
-    <div>123</div>
+    <div>
+      <Link to={ROUTE_FLIP_CARD}>Link cards</Link>
+      <br />
+      <Link to={ROUTE_BOOK_CARD}>Book cards</Link>
+    </div>
   )
 }
