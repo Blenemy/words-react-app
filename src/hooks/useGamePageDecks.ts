051ecb7ddaa37fa0b98864c6432e00248c2989a3
Denys Topchyi <miliarder97@gmail.com>
2023-10-30 00:47:07 +0200

    ref: deleted unused code

--- src/hooks/useGamePageDecks.ts
+++ src/hooks/useGamePageDecks.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { DeckFromServer } from "../types/DeckFromServer";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ROUTE_AUTHORIZATION } from "../data/constants";
 import { getDecks } from "../api/getDeck";
@@ -22,9 +21,6 @@ export const useGamePageDecks = () => {
     queryFn: () => getDecks(token),
     queryKey: ["deck"],
   });
-
-  // const defaultDecks: DeckFromServer[] =
-  //   query.data?.filter((deck: DeckFromServer) => deck.default) || null;
 
   return { defaultDecks, navigate, token };
 };
