5378cdc591b258a774ef68f02ec756a20aee53d4
Denys Topchyi <miliarder97@gmail.com>
2023-11-01 18:37:41 +0200

    bug: fix bug with redirection, use replace value

--- src/hooks/useGamePageDecks.ts
+++ src/hooks/useGamePageDecks.ts
@@ -1,25 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { ROUTE_AUTHORIZATION } from "../data/constants";
+import { useNavigate } from "react-router-dom";
+import { ROUTE_NOT_AVAILABLE } from "../data/constants";
 import { getDecks } from "../api/getDeck";
 import Cookies from "js-cookie";
 import { DeckFromServer } from "../types/DeckFromServer";
+import { useAppSelector } from "../app/hooks";
 
 export const useGamePageDecks = () => {
   const token = Cookies.get("token");
+  const { user } = useAppSelector((state) => state.user);
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
-    if (!token) {
-      navigate(ROUTE_AUTHORIZATION, { state: { from: location.pathname } });
+    if (!user) {
+      navigate(ROUTE_NOT_AVAILABLE, { replace: true });
       return;
     }
-  }, [token, navigate, location.pathname]);
+  }, [user, navigate]);
 
   const { data } = useQuery({
     queryFn: () => getDecks(token),
+    enabled: !!token,
     queryKey: ["decks"],
   });
 
