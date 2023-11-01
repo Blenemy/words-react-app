ba4f8329f4b4b53a291a917c874379a16c571bc6
Denys Topchyi <miliarder97@gmail.com>
2023-11-01 18:44:30 +0200

    feat: change notAvailable comp to 403 access denied

--- src/components/NotAvailable/NotAvailable.tsx
+++ src/components/NotAvailable/NotAvailable.tsx
@@ -1,38 +1,82 @@
 import { Link } from "react-router-dom";
-import { ROUTE_HOME } from "../../data/constants";
+import {
+  ROUTE_AUTHORIZATION,
+  ROUTE_HOME,
+  ROUTE_REGISTRATION,
+} from "../../data/constants";
 
 export const NotAvailable = () => {
   return (
     <div className="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
       <div className="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
         <p className="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-gray-300">
-          404
+          403
         </p>
         <p className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-300 mt-2">
-          Page Not Found
+          Access Denied
         </p>
         <p className="text-lg md:text-xl lg:text-2xl text-gray-500 my-12">
-          Sorry, the page you are looking for could not be found.
+          Sorry, this page is available for registered users only.
         </p>
-        <Link
-          to={ROUTE_HOME}
-          className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
-          title="Return Home"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <div className="flex gap-6">
+          <Link
+            to={ROUTE_HOME}
+            className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
+            title="Return Home"
           >
-            <path
-              fillRule="evenodd"
-              d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span>Return Home</span>
-        </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span>Return Home</span>
+          </Link>
+          <Link
+            to={ROUTE_AUTHORIZATION}
+            className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
+            title="Return Home"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span>Sign in</span>
+          </Link>
+          <Link
+            to={ROUTE_REGISTRATION}
+            className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
+            title="Return Home"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span>Sign up</span>
+          </Link>
+        </div>
       </div>
       <div className="w-1/2 lg:h-full flex lg:items-end justify-center p-4">
         <svg
