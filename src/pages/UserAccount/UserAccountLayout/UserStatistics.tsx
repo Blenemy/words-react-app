720ee86bc17b507a5402f7acc0d3e5863b95abaf
Denys Topchyi <miliarder97@gmail.com>
2023-11-07 01:51:46 +0200

    feat: add brush to the graph, improved Y axis showout

--- src/pages/UserAccount/UserAccountLayout/UserStatistics.tsx
+++ src/pages/UserAccount/UserAccountLayout/UserStatistics.tsx
@@ -6,9 +6,12 @@ import {
   Tooltip,
   CartesianGrid,
   ResponsiveContainer,
+  Brush,
 } from "recharts";
 import { useAppSelector } from "../../../app/hooks";
 import { memo, useMemo } from "react";
+import { format } from "date-fns";
+import { enUS } from "date-fns/locale";
 
 /**
  * Компонент UserStatistics для отображения линейного графика прогресса обучения пользователя.
@@ -23,21 +26,33 @@ export const UserStatisctics = memo(() => {
 
   const formattedData = useMemo(() => {
     if (user) {
-      return Object.keys(user?.progress)
+      const lastSevenEntries = Object.keys(user?.progress)
         .sort()
         .map((date) => ({
-          date,
+          date: format(new Date(date), "MMM d", { locale: enUS }),
           progress: user?.progress[date],
         }));
+      return lastSevenEntries;
     }
   }, [user]);
+
+  const maxYValue = useMemo(() => {
+    return formattedData
+      ? Math.max(...formattedData.map((data) => data.progress), 10)
+      : 50;
+  }, [formattedData]);
 
   return (
     <div className="text-primary basis-3/5">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart width={800} height={420} data={formattedData}>
           <XAxis dataKey="date" stroke="#444" />
-          <YAxis stroke="#444" domain={[0, 50]} tickCount={6} interval={0} />
+          <YAxis
+            stroke="#444"
+            domain={[0, maxYValue]}
+            tickCount={6}
+            interval={0}
+          />
           <Tooltip wrapperStyle={{ backgroundColor: "#ccc" }} />
           <Line
             type="monotone"
@@ -47,6 +62,7 @@ export const UserStatisctics = memo(() => {
             dot={false}
           />
           <CartesianGrid stroke="#ccc" strokeDasharray="0 0" vertical={false} />
+          <Brush dataKey="date" height={20} stroke="#8884d8" />
         </LineChart>
       </ResponsiveContainer>
     </div>
